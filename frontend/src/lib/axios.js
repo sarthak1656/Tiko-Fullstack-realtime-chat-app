1058d1d840f4c5cdd715b923bf8aa7b7da1c132b
Sarthak Panigrahi <sipunp70@gmail.com>
2025-04-08 23:49:27 +0530

    final fixes

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
+  baseURL: import.meta.env.MODE === "development"
+    ? "http://localhost:5001/api"
+    : "https://tiko-fullstack-realtime-chat-app.onrender.com/api", // ✅ Add this
   withCredentials: true,
 });
+
